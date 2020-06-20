750067083aa00636dd6989427fc43f3909aa332c
Erik Neville <erikeneville@gmail.com>
2020-06-20 00:58:27 -0700

    updated after cvd change

--- src/screens/Events/EventsScreen.js
+++ src/screens/Events/EventsScreen.js
@@ -9,29 +9,6 @@ const EventsScreen = ({ events }) => {
   return (
     <BodyContainer>
       <ScreenContainer>
-        <br />
-        <h4>
-          Redgate has suspended its evening concerts until the weather improves
-          enough to open up the Fieldhouse garage doors, therefore providing
-          more space for social distancing. We will however, reinstate our "Solo
-          Sundays" concerts.
-        </h4>
-        <h4>
-          We will also start <strong>Solo Saturdays!</strong> Our intention is
-          twofold:
-        </h4>
-        <h4>
-          First, we want to get some bread to our family of Redgate
-          entertainers.
-        </h4>
-        <h4>
-          Second, we want to get the public comfortable coming out to our venue
-          in a low key atmosphere with limited seating, social distancing, bring
-          your own picnic, daylight operations, and the knowledge that that
-          their exposure is managed with the best known methods. Outdoor seating
-          available depending on the weather.
-        </h4>
-        <br />
         <EventList event={june} />
         <EventList event={july} />
         <EventList event={august} />
